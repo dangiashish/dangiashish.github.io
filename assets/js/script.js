f2bce7b7d35c304dfcc6b69b2737b3936897099f
Ashish <ashishdangi96@gmail.com>
2023-11-21 15:43:20 +0530

    responsive ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,11 @@ const navToggleFunc = function () { nav.classList.toggle('active'); }
 navMenuBtn.addEventListener('click', navToggleFunc);
 navCloseBtn.addEventListener('click', navToggleFunc);
 
-function openNewtab(url){
+function openNewtab(url) {
   window.open(url, '_blank');
 }
 // theme toggle variables
 const themeBtn = document.querySelectorAll('.theme-btn');
-
 
 for (let i = 0; i < themeBtn.length; i++) {
 
@@ -41,12 +40,26 @@ for (let i = 0; i < themeBtn.length; i++) {
 
 }
 
-const links = document.querySelectorAll('a');
+// const links = document.querySelectorAll('a');
 
-links.forEach(link => {
-  link.addEventListener('click', function (event) {
-    event.preventDefault(); // Prevents the default action of following the link
-    const url = this.getAttribute('href'); // Get the URL from the clicked link
-    openNewtab(url); // Open the URL in a new tab
+// links.forEach(link => {
+//   link.addEventListener('click', function (event) {
+//     event.preventDefault(); // Prevents the default action of following the link
+//     const url = this.getAttribute('href'); // Get the URL from the clicked link
+//     openNewtab(url); // Open the URL in a new tab
+//   });
+// });
+
+
+
+let includes = document.getElementsByTagName('include');
+for (var i = 0; i < includes.length; i++) {
+  let include = includes[i];
+  load_file(includes[i].attributes.src.value, function (text) {
+    include.insertAdjacentHTML('afterend', text);
+    include.remove();
   });
-});
+}
+function load_file(filename, callback) {
+  fetch(filename).then(response => response.text()).then(text => callback(text));
+}
