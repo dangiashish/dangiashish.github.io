f554e63b31f29df615a24796af9b7901010ba622
Ashish <ashishdangi96@gmail.com>
2023-12-23 20:50:51 +0530

    new lib added

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,3 +63,25 @@ for (var i = 0; i < includes.length; i++) {
 function load_file(filename, callback) {
   fetch(filename).then(response => response.text()).then(text => callback(text));
 }
+
+
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds (2000 milliseconds)
+
+}
+
+
+
